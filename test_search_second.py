75756891b22f7d77aa3c97dbb20c0bc95adae379
hatsii <hets48rus@mail.ru>
2025-09-11 18:42:43 +0300

    Delete test_search_second.py

--- test_search_second.py
+++
@@ -1,8 +0,0 @@
-from selene import browser, be, have
-
-
-def test_search_positive(size_browser):
-    browser.open('https://duckduckgo.com/')
-    browser.element('[id="searchbox_input"]').should(be.blank).type('qa.guru').press_enter()
-    browser.element('//span[contains(text(), "qa guru")]')
-
