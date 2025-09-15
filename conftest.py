50ac7383779139ed43463f781ba1a7de62e1c8df
Vadim Tatarnikov <v.tatarnikov@adengi.ru>
2025-09-15 21:54:39 +0300

    Add test form

--- conftest.py
+++ conftest.py
@@ -4,11 +4,9 @@ from selene import browser
 
 
 @pytest.fixture(scope="function")
-def size_browser():
+def browser_open_page():
     browser.config.window_height = 1080
     browser.config.window_width = 1920
-
+    browser.open("https://demoqa.com/automation-practice-form")
     yield
-
-
     browser.quit()
